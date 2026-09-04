ce342b2ac5a6efcde60837ebbc02cd1204eb608e
ydh1121 <ydh1121@naver.com>
2026-09-04 22:09:52 +0900

    feat: wire resilient BEA actual capture

--- b3_trader/intelligence_ingest_cycle.py
+++ b3_trader/intelligence_ingest_cycle.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from typing import Callable, Iterable
 
 from .auto_demo_v2 import DB_PATH
-from .intelligence_bea_actual import BeaActualCaptureService
+from .intelligence_bea_actual_resilient import BeaActualCaptureService
 from .intelligence_bea_schedule import BeaReleaseScheduleSource
 from .intelligence_bls_actual_resilient import BlsActualCaptureService
 from .intelligence_bls_calendar_resilient import ResilientBlsReleaseCalendarSource
